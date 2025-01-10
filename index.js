912facdda6e2fdc4e41eca4f0c54c613da86e921
Abduraxim <toxirmalk@gmail.com>
2025-01-10 16:44:47 +0500

    nothing

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const express=require("express")
+const express = require("express");
 
+const app = express();
 
-const app=express()
+app.use(express.json());
 
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Salom alaykum");
+});
 
+app.use("/", (req, res) => {
+  res.send("Salom alaykum");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Salom alaykum")
-})
-
-app.use("/",(req,res)=>{
-    res.send("Salom alaykum")
-})
-
-
-app.listen(6000,()=>{
-    console.log("Server is running on port 6000")
-})
-
+app.listen(6000, () => {
+  console.log("Server is running on port 6000");
+});
