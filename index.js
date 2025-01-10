db695b11c9f36d405c370cdb10ffae0a2b7154ad
Abduraxim <toxirmalk@gmail.com>
2025-01-10 16:21:25 +0500

    Mini repositrort

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+
+
+const app=express()
+
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+    res.send("Salom alaykum")
+})
+
+app.use("/",(req,res)=>{
+    res.send("Salom alaykum")
+})
+
+
+app.listen(4000,()=>{
+    console.log("Server is running on port 4000")
+})
+
