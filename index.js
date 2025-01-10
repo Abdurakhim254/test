b3af272c22dd5fbd9de992416d9d5d0b91838cac
Abduraxim <toxirmalk@gmail.com>
2025-01-10 18:19:45 +0500

    some changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import  express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Salom Alaykum Uzbekiston");
+  res.send("Salom Alaykum Uzbekiston olga vatanim");
 });
 
 app.use("/", (req, res) => {
